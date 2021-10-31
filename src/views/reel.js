704ac5334dcfb9ddac0181cec359ab4792451fb6
Tonya Fletcher <tfletch4504@gmail.com>
2021-10-31 19:21:23 -0400

    add back buttons to film

--- src/views/reel.js
+++ src/views/reel.js
@@ -1,9 +1,9 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
 import OmdbContainer from "../components/OmdbContainer";
-// import home from "../images/home.jpg";
 import "../index.css";
 
 function Reel() {
@@ -15,7 +15,13 @@ function Reel() {
                         <h1 className="text-center">Reel</h1>
                     </Col>
                 </Row>
-                
+                <Row>
+                    <div className="text-center">
+                        <Link to="/film">
+                            <button className="btn-view">Go Back</button>
+                        </Link>
+                    </div>
+                </Row>
             </Container>
 
             <Container>
