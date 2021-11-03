e1ba25a65d0837a77672832e67c2d140da55212d
Tonya Fletcher <tfletch4504@gmail.com>
2021-11-03 12:16:29 -0400

    remove redundant code

--- src/components/VideoPlayer2/index.js
+++ src/components/VideoPlayer2/index.js
@@ -1,6 +1,6 @@
 // import React from "react";
 // import { ReactVideo } from "reactjs-media";
-// import pviff from "../../images/pviff.MP4";
+// // import pviff from "../../images/pviff.MP4";
 // import pviffcover from "../../images/pviffcover.png";
 
 // class VideoPlayer2 extends React.Component {
@@ -15,7 +15,8 @@
 
 //         return <ReactVideo
 //             opts={opts}
-//             src={pviff} alt="demo video"
+//             // src={pviff} "" alt="demo video"
+//             // src="https://cdn.api.video/vod/vi6grZKLExHTFpNK3plvKSXC/mp4/1080/source.mp4"
 //             poster={pviffcover}
 //             primaryColor="var(--sky)"
 //         />
